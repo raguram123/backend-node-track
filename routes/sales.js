5745e06aa211d7b78cc0c8ecdfdd91b97656dfb3
raguram123 <137371586+raguram123@users.noreply.github.com>
2023-11-30 12:31:59 +0530

    Update sales.js

--- routes/sales.js
+++ routes/sales.js
@@ -1,1 +1,33 @@
+import express, { Router } from "express"
+import { getAllProducts } from "../controllers/sales.js";
+import sales from "../models/sales.js";
 
+const router =  express.Router();
+
+//Get All Products 
+router.get("/all", async (req, res) => {
+
+    try {
+        const Product = await  getAllProducts();
+        if (!Product) return res.status(401).json({message: 'No products found'});
+
+        res.status(200).json(
+            {
+                data : product,
+            }
+        )
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(
+            {
+                error: "Internal Server"
+            }
+        )
+        
+    }
+
+})
+
+
+
+export const productRouter = router;
